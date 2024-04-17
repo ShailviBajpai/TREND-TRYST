02d42c27700b6041133228f8eb9095f1dbef7e84
Shailvi Bajpai <shailvibajpai13@gmail.com>
2024-04-17 18:47:00 +0530

    final

---
+++ src/components/Main.js
@@ -0,0 +1,108 @@
+import React from "react";
+import Cart from "./content page/Cart";
+import Men from "./content page/Men";
+import Wishlist from "./content page/Wishlist";
+import Women from "./content page/Women";
+import Home from "./content page/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Login from "../components/Login";
+ import SignUp from "../components/Signup";
+
+export default function Main({
+    addToCart,
+    toggleWishlist,
+    incrementQuantity,
+    decrementQuantity,
+    removeFromCart,
+    clearCart,
+}) {
+    console.log("main");
+    return (
+        <>
+            <BrowserRouter basename="/shopping-cart/">
+                <Routes>
+                    <Route
+                        path="/login"
+                        element={
+                            <>
+                                <Header />
+                                <Login
+                                />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/signup"
+                        element={
+                            <>
+                                <Header />
+                                <SignUp
+                                />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <Header />
+                                <Home />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/men"
+                        element={
+                            <>
+                                <Header />
+                                <Men
+                                    addToCart={addToCart}
+                                    toggleWishlist={toggleWishlist}
+                                />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/women"
+                        element={
+                            <>
+                                <Header />
+                                <Women
+                                    addToCart={addToCart}
+                                    toggleWishlist={toggleWishlist}
+                                />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/checkout"
+                        element={
+                            <>
+                                <Header />
+                                <Cart
+                                    incrementQuantity={incrementQuantity}
+                                    decrementQuantity={decrementQuantity}
+                                    removeFromCart={removeFromCart}
+                                    clearCart={clearCart}
+                                />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/wishlist"
+                        element={
+                            <>
+                                <Header />
+                                <Wishlist
+                                    addToCart={addToCart}
+                                    toggleWishlist={toggleWishlist}
+                                />
+                            </>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
